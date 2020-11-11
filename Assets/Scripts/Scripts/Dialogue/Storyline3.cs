a19e17fc2c923014d86afdc3f1b9541e07c9f04b
seanpohyx <seanpohyx@gmail.com>
2020-11-12 02:07:14 +0800

    fixed errors and include bg music for lvl3

---
+++ Assets/Scripts/Scripts/Dialogue/Storyline3.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Storyline3 : MonoBehaviour
+{
+    public Dialogue sd, repeatD;
+
+    void Start()
+    {
+        StartCoroutine(callStartDialog());
+    }
+
+    private IEnumerator callStartDialog()
+    {
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Callinng Start");
+        int timesPlayed = PlayerPrefs.GetInt("times_played", 0);
+        if (timesPlayed <= 0)
+            FindObjectOfType<DialogueManager>().StartDialogue(sd);
+        else
+        {
+            FindObjectOfType<DialogueManager>().StartDialogue(repeatD);
+        }
+
+    }
+
+    private void Update()
+    {
+
+
+    }
+}
